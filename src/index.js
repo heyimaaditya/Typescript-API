1eb59ec2500f8203746c236121428d18a689ef27
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-17 17:25:11 +0530

    added:Create User Table

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import dotenv from 'dotenv';
 import pool from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import errorHandling from './middlewares/errorhandler.js';
+import createUserTable from './data/createUserTable.js';
 dotenv.config();
 const app=express();
-const PORT=process.env.PORT || 3001;
+const PORT=5001;
 app.use(cors());
 app.use(express.json());
 app.use('/api',userRoutes);
 app.use(errorHandling);
+createUserTable();
 app.get('/',async(req,res)=>{
     const result=await pool.query('SELECT current_database()');
     res.send(`Database: ${result.rows[0].current_database}`);
@@ -18,4 +20,4 @@ app.get('/',async(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-});   
+});
