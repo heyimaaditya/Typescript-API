fb1ebd4aa33cce85dd099ab8177c0c3d1ee0d64d
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-17 17:29:30 +0530

    feat:added inputValidator using joi for schema Validation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createUser,deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/userController.js';
+import validateUser from '../middlewares/inputValidator.js';
 const router=express.Router();
-router.post("/user",createUser);
+router.post("/user",validateUser,createUser);
 router.get("/user",getAllUsers);
 router.get("/user/:id",getUserById);
-router.put("/user/:id",updateUser);
-router.delete("/user/:id",deleteUser);
+router.put("/user/:id",validateUser,updateUser);
+router.delete("/user/:id",validateUser,deleteUser);
 export default router;
